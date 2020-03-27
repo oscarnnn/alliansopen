26a13ec05e62f28edbcf347c3eff3e179570846e
oscarnnn <35729517+oscarnnn@users.noreply.github.com>
2020-03-27 08:08:27 +0100

    change title

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -10,7 +10,7 @@ const toolbar = props => (
             <div className="toolbar__toggle-button">
                 <DrawerToggleButton click={props.drawerClickHandler}/>
             </div>
-            <div className="toolbar__logo"><a href="/">Allians Max Open 2020</a></div>
+            <div className="toolbar__logo"><a href="/">Allians Max Tour 2020</a></div>
             <div className="spacer"></div>
             <div className="toolbar_navigation-items">
                 <ul>
