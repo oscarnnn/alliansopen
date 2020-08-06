2836753a92f51e524301ca3087e72688021a7fdc
oscarnnn <35729517+oscarnnn@users.noreply.github.com>
2020-08-06 19:33:00 +0200

    add title to iframe

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -4,7 +4,7 @@ const Intro = () => {
 
   return (
     <div className="iframe">
-      <iframe width="893" height="502" src="https://www.youtube.com/embed/vq5evQkb_0w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"
+      <iframe title="intro" width="893" height="502" src="https://www.youtube.com/embed/vq5evQkb_0w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"
         mozallowfullscreen="mozallowfullscreen" 
         msallowfullscreen="msallowfullscreen" 
         oallowfullscreen="oallowfullscreen" 
