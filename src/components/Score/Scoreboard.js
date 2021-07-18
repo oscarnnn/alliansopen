f6489f7bf07dff82a9618de2d2a87c9c59f730f4
Oscar Nilsson <oscruss@hotmail.com>
2021-07-18 11:01:33 +0200

    update score

--- src/components/Score/Scoreboard.js
+++ src/components/Score/Scoreboard.js
@@ -18,11 +18,56 @@ const scoreBoard = () => (
         <tr>
           <th scope="row">Anton Adielsson</th>
           <td>95/27</td>
-          <td>0/0</td>
+          <td>84/41</td>
           <td>0/0</td>
           <td>1</td>
-          <td className="table-success">95</td>
-          <td className="table-success">29</td>
+          <td className="table-success">179</td>
+          <td className="table-success">70</td>
+        </tr>
+        <tr>
+          <th scope="row">Jesper Leinonen</th>
+          <td>106/25</td>
+          <td>98/36</td>
+          <td>0/0</td>
+          <td>1</td>
+          <td className="table-success">204</td>
+          <td className="table-success">63</td>
+        </tr>
+        <tr>
+          <th scope="row">Axel Esborn</th>
+          <td>107/23</td>
+          <td>92/35</td>
+          <td>0/0</td>
+          <td>0</td>
+          <td className="table-success">199</td>
+          <td className="table-success">58</td>
+        </tr>
+        <tr>
+          <th scope="row">Joakim Hammarström</th>
+          <td>86/27</td>
+          <td>84/27</td>
+          <td>0/0</td>
+          <td>1</td>
+          <td className="table-success">170</td>
+          <td className="table-success">56</td>
+        </tr>
+        <tr>
+          <th scope="row">Oscar Nilsson</th>
+          <td>106/21</td>
+          <td>89/33</td>
+          <td>0/0</td>
+          <td>1</td>
+          <td className="table-success">195</td>
+          <td className="table-success">56</td>
+        </tr>
+        <tr>
+          <th scope="row">Patrick Marklund</th>
+          <td>118/25</td>
+          <td>115/25</td>
+          <td>0/0</td>
+          <td>1</td>
+          <td className="table-success">233</td>
+          <td className="table-success">52</td>
         </tr>
         <tr>
           <th scope="row">Martin Hansson</th>
@@ -34,58 +79,13 @@ const scoreBoard = () => (
           <td className="table-success">28</td>
         </tr>
         <tr>
-          <th scope="row">Joakim Hammarström</th>
-          <td>86/27</td>
-          <td>0/0</td>
-          <td>0/0</td>
-          <td>0</td>
-          <td className="table-success">86</td>
-          <td className="table-success">27</td>
-        </tr>
-        <tr>
-          <th scope="row">Patrick Marklund</th>
-          <td>118/25</td>
-          <td>0/0</td>
-          <td>0/0</td>
-          <td>1</td>
-          <td className="table-success">118</td>
-          <td className="table-success">27</td>
-        </tr>
-        <tr>
-          <th scope="row">Jesper Leinonen</th>
-          <td>106/25</td>
-          <td>0/0</td>
-          <td>0/0</td>
-          <td>0</td>
-          <td className="table-success">106</td>
-          <td className="table-success">25</td>
-        </tr>
-        <tr>
-          <th scope="row">Axel Esborn</th>
-          <td>107/23</td>
-          <td>0/0</td>
-          <td>0/0</td>
-          <td>0</td>
-          <td className="table-success">107</td>
-          <td className="table-success">23</td>
-        </tr>
-        <tr>
-          <th scope="row">Oscar Nilsson</th>
-          <td>106/21</td>
-          <td>0/0</td>
-          <td>0/0</td>
-          <td>0</td>
-          <td className="table-success">106</td>
-          <td className="table-success">21</td>
-        </tr>
-        <tr>
           <th scope="row">Jonathan Karrin</th>
           <td>0/0</td>
-          <td>0/0</td>
+          <td>132/17</td>
           <td>0/0</td>
           <td>0</td>
-          <td className="table-success">0</td>
-          <td className="table-success">0</td>
+          <td className="table-success">132</td>
+          <td className="table-success">17</td>
         </tr>
       </tbody>
     </table>
